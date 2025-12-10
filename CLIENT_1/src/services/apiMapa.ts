670ed967f2ea4e3d77f9febcdff85f02fb78465a
juanes0uu <juanestebanstt@gmail.com>
2025-12-10 10:17:35 -0500

    CAMBIOS

--- CLIENT_1/src/services/apiMapa.ts
+++ CLIENT_1/src/services/apiMapa.ts
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:8080";
+const API_BASE = import.meta.env.VITE_API_BASE_URL ?? "https://nontesting-unindicatively-nicolle.ngrok-free.dev";
 
 async function request<T>(path: string, opts: RequestInit = {}): Promise<T> {
   const res = await fetch(`${API_BASE}${path}`, {
--- CLIENT_1/vite.config.ts
+++ CLIENT_1/vite.config.ts
@@ -1,7 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Importante para que acepte conexiones externas/de red
+    port: 5173, // Define explícitamente el puerto que usarás con Cloudflared
+    strictPort: true,
+    watch: {
+      usePolling: true,
+    },
+    allowedHosts: [
+      '.trycloudflare.com' // Permite cualquier subdominio de Cloudflare Tunnel
+    ],
+  },
 })
